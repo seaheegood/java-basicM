e4ab67e5e662a531dc93d86a98b6148f80a302ac
Sehee Hong <seaheehong@gmail.com>
2024-02-20 20:59:09 +0900

    김영한 자바 기본편 - 자바 메모리 구조(memory structure)와 static

---
+++ src/memory/JavaMemoryMain2.java
@@ -0,0 +1,31 @@
+package memory;
+
+import static memory.JavaMemoryMain1.method2;
+
+public class JavaMemoryMain2 {
+    public static void main(String[] args) {
+        System.out.println("main start");
+        method1();
+        System.out.println("main end");
+    }
+
+    static void method1() {
+        System.out.println("method1 start");
+        Data data1 = new Data(10);
+        method2(data1);
+        System.out.println("method1 end");
+    }
+
+    static void method2(Data data2) {
+        System.out.println("method2 start");
+        System.out.println("data.value = " + data2.getValue());
+        System.out.println("method2 end");
+    }
+}
+/*
+정리
+지역 변수는 스택 영역에, 객체(인스턴스)는 힙 영역에 관리되는 것을 확인했다. 이제 나머지 하나가 남았다.
+바로 메서드 영역이다. 메서드 영역이 관리하는 변수도 있다. 이것을 이해하기 위해서는 먼저
+static 키워드를 알아야한다.
+static 키워드는 메서드 영역과 밀접한 연관이 있다.
+ */
