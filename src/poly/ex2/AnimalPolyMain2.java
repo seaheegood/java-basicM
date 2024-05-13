50128803d19f7a79b03ea45d51475fe2322b7044
Sehee Hong <seaheehong@gmail.com>
2024-05-12 23:28:21 -0400

    김영한 자바 기본편 - 다형성 활용 1,2,3

---
+++ src/poly/ex2/AnimalPolyMain2.java
@@ -0,0 +1,20 @@
+package poly.ex2;
+
+public class AnimalPolyMain2 {
+
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+        Caw caw = new Caw();
+        Duck duck = new Duck();
+        Animal[] animalArray = {dog, cat, caw, duck};
+//        Animal index1 = dog;
+//        Animal index2 = cat;
+//        Animal index3 = caw;
+        for (Animal animal : animalArray) {
+            System.out.println("동물 소리 테스트 시작");
+            animal.sound();
+            System.out.println("동물 소리 테스트 종료");
+        }
+    }
+}
