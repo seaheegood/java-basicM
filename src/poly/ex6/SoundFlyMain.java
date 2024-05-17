dbc85e2b57c7b4b74f3fbd1d8e65ebd50c8c3f44
Sehee Hong <seaheehong@gmail.com>
2024-05-17 00:49:40 -0400

    김영한 자바 기본편 - 클래스와 인터페이스 활용

---
+++ src/poly/ex6/SoundFlyMain.java
@@ -0,0 +1,32 @@
+package poly.ex6;
+
+public class SoundFlyMain {
+
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        Bird bird = new Bird();
+        Chicken chicken = new Chicken();
+
+        soundAnimal(dog);
+        soundAnimal(bird);
+        soundAnimal(chicken);
+
+//        flyAnimal(dog); 상위 타입에 Fly 가 없기 때문에 사용 불가능
+        flyAnimal(bird);
+        flyAnimal(chicken);
+    }
+
+    //AbstractAnimal 사용 가능
+    private static void soundAnimal(AbstractAnimal animal) {
+        System.out.println("동물 소리 테스트 시작");
+        animal.sound();
+        System.out.println("동물 소리 테스트 종료");
+    }
+
+    //Fly 인터페이스가 있으면 사용 가능
+    private static void flyAnimal(Fly fly) {
+        System.out.println("날기 테스트 시작");
+        fly.fly();
+        System.out.println("날기 테스트 종료");
+    }
+}
